d2aab339f4bc93b39d8ab4e6d42830d1ade718ce
LorenzoM27 <lorenzo.menino@lacatholille.fr>
2024-10-21 12:08:40 +0200

    finish

---
+++ TajMahal/TajMahal/Components/DetailInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailInfoView.swift
+//  TajMahal
+//
+//  Created by Lorenzo Menino on 21/10/2024.
+//
+
+import SwiftUI
+
+struct DetailInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailInfoView()
+}
